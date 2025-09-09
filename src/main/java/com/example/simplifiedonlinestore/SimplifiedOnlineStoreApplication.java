7102329bf9e0f5c77152dabba642e4e2d0603a49
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-09-09 21:18:28 +0300

    feat: Init

---
+++ src/main/java/com/example/simplifiedonlinestore/SimplifiedOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplifiedonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedOnlineStoreApplication.class, args);
+	}
+
+}
